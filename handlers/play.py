69c608186e2c6e43c88aa2eb475b4301c071170b
KarabakhMusic <106437032+KarabakhMusic@users.noreply.github.com>
2022-06-08 19:52:23 +0400

    Update play.py

--- handlers/play.py
+++ handlers/play.py
@@ -109,7 +109,7 @@ async def play(_, message: Message):
     try:
         user = await USER.get_me()
     except:
-        user.first_name = "TaliaMusicAsistant"
+        user.first_name = "kafkazmusicasistan"
     usar = user
     wew = usar.id
     try:
@@ -198,8 +198,8 @@ async def play(_, message: Message):
             keyboard = InlineKeyboardMarkup(
         [
             [
-                InlineKeyboardButton("🚀 İnfo", url=f"https://t.me/efsanestar_bot"),
-                InlineKeyboardButton("📣 Kanal", url=f"https://t.me/Sohbetdestek"),
+                InlineKeyboardButton("🚀 İnfo", url=f"https://t.me/Kafkazmusicbot"),
+                InlineKeyboardButton("📣 Kanal", url=f"https://t.me/azeqafqaz2021"),
             ],[
                 InlineKeyboardButton("📲 Kapat", callback_data="cls"),
             ],
@@ -263,8 +263,8 @@ async def play(_, message: Message):
         keyboard = InlineKeyboardMarkup(
         [
             [
-                InlineKeyboardButton("🚀 İnfo", url=f"https://t.me/efsanestar_bot"),
-                InlineKeyboardButton("📣 Kanal", url=f"https://t.me/Sohbetdestek"),
+                InlineKeyboardButton("🚀 İnfo", url=f"https://t.me/kafkazmusicbot"),
+                InlineKeyboardButton("📣 Kanal", url=f"https://t.me/azeqafqaz2021"),
             ],[
                 InlineKeyboardButton("📲 Kapat", callback_data="cls"),
             ],
